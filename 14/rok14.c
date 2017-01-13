afbca6df4cf5306541a427cd9284fa42d896498f
Andrej Jokic <aj.jokic@gmail.com>
2017-01-13 16:42:20 +0100

    remove unnecessary stuff

--- 14/rok14.c
+++ 14/rok14.c
@@ -4,50 +4,37 @@ int max(int*,int);
 int main(){
     FILE *f = fopen("Visine.txt", "r");
     int n[50], i=0,j, rez=0, tmp=0;
-    printf("Ucitane visine: ");
     while(!feof(f)){
         fscanf(f, "%d", &n[i++]);
-        printf(" %d",n[i-1]);//
-    }printf("\n");//
+    }
     int br = i;
-    printf("br: %d\n", br);//
+
     int lijeva=0;
-    int mx = max(n,br);
-    char mat[mx][br];
-    printf("mx: %d\n",mx);//
-    for(i=0;i<mx;i++){//red
+    int mx = max(n,br);    
+
+    for(i=0;i<mx;i++){//visina
         //Krenem od najnizeg reda, postavljam lijevu granicu
         lijeva=-1; tmp=0;
         for(j=0;j<br;j++){//niz
-            mat[i][j]=' ';
             if(lijeva==-1){
                 if(n[j]>i){
                     lijeva=j;
-                    mat[i][j]='#';}
+                }
             }
             else{
                 if(n[j]>i){
-                    //lijeva=-1;
-                    mat[i][j]='#';
                     rez+=tmp;
-                    while(tmp>0)mat[i][j-tmp--]='-';
                     tmp=0;
                 }
                 else {
                     tmp++;
-                    //mat[i][j]='-';
                 }
             }
         }
     }
-    
+
     printf ("Kockice vode: %d\n",rez);
-    printf ("Bonus - slika: \n");//nije potrebno
-    for(i=mx;i>=0;i--){
-        for(j=0;j<br;j++){
-            printf("%c",mat[i][j]);
-        }printf("\n");
-    }
+
     printf("\n");
     fclose(f);
     return 0;
